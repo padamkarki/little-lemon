eba277331919105b37bfa323a4fba0a0fc2e5f26
Padam Karki <padamkarkipkkp@gmail.com>
2023-01-17 15:04:11 +0530

    removed default app css file

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import Header from "./component/Header";
 import Main from "./component/Main";
 import Footer from "./component/Footer";
 
-import "./App.css";
-
 function App() {
   return (
     <>
-      <div className="App">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
