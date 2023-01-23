53d49daf3803ea377d90fa2eeaaad828dd02c261
Padam Karki <padamkarkipkkp@gmail.com>
2023-01-23 17:21:51 +0530

    completed homepage design

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,8 +1,61 @@
+import restaurant_img from "../images/restaurant.jpg";
+
+import styles from "./Footer.module.css";
+
 const Footer = () => {
   return (
     <>
-      <footer>
-        <p>Footer</p>
+      <footer className={styles.section}>
+        <div className={styles.footer_flex}>
+          <div>
+            <img
+              className={styles.img}
+              src={restaurant_img}
+              alt="restaurant_img"
+            />
+          </div>
+          <div>
+            <h3>
+              DOORMAT
+              <br />
+              NAVIGATION
+            </h3>
+            <ul>
+              <li className={styles.links}>
+                <a href="/#">Home</a> <br />
+                <a href="/#">About</a> <br />
+                <a href="/#">Menu</a> <br />
+                <a href="/#">Reservation</a> <br />
+                <a href="/#">Order Online</a> <br />
+                <a href="/#">Login</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>CONTACTS</h3>
+            <ul>
+              <li className={styles.links}>
+                <a href="/#">Addrses</a>
+                <br />
+                <a href="/#">Phone Number</a>
+                <br />
+                <a href="/#">Email</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>SOCIAL MEDIA LINKS</h3>
+            <ul>
+              <li className={styles.links}>
+                <a href="/#">Addrses</a>
+                <br />
+                <a href="/#">Phone Number</a>
+                <br />
+                <a href="/#">Email</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </footer>
     </>
   );
