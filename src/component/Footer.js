d8e28610b2d932328953be05c17150b2fd0f8f77
Padam Karki <padamkarkipkkp@gmail.com>
2023-02-03 17:53:35 +0530

    added layout and changed ui

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,54 +6,56 @@ const Footer = () => {
   return (
     <>
       <footer className={styles.section}>
-        <div className={styles.footer_flex}>
-          <div>
-            <img
-              className={styles.img}
-              src={restaurant_img}
-              alt="restaurant_img"
-            />
-          </div>
-          <div>
-            <h3>
-              DOORMAT
-              <br />
-              NAVIGATION
-            </h3>
-            <ul>
-              <li className={styles.links}>
-                <a href="/#">Home</a> <br />
-                <a href="/#">About</a> <br />
-                <a href="/#">Menu</a> <br />
-                <a href="/#">Reservation</a> <br />
-                <a href="/#">Order Online</a> <br />
-                <a href="/#">Login</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3>CONTACTS</h3>
-            <ul>
-              <li className={styles.links}>
-                <a href="/#">Addrses</a>
+        <div className={styles.footer_space}>
+          <div className={styles.footer_flex}>
+            <div>
+              <img
+                className={styles.img}
+                src={restaurant_img}
+                alt="restaurant_img"
+              />
+            </div>
+            <div>
+              <h3>
+                DOORMAT
                 <br />
-                <a href="/#">Phone Number</a>
-                <br />
-                <a href="/#">Email</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3>SOCIAL MEDIA LINKS</h3>
-            <ul>
-              <li className={styles.links}>
-                <a href="/#">Addrses</a>
-                <br />
-                <a href="/#">Phone Number</a>
-                <br />
-                <a href="/#">Email</a>
-              </li>
-            </ul>
+                NAVIGATION
+              </h3>
+              <ul>
+                <li className={styles.links}>
+                  <a href="/#">Home</a> <br />
+                  <a href="/#">About</a> <br />
+                  <a href="/#">Menu</a> <br />
+                  <a href="/#">Reservation</a> <br />
+                  <a href="/#">Order Online</a> <br />
+                  <a href="/#">Login</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3>CONTACTS</h3>
+              <ul>
+                <li className={styles.links}>
+                  <a href="/#">Addrses</a>
+                  <br />
+                  <a href="/#">Phone Number</a>
+                  <br />
+                  <a href="/#">Email</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3>SOCIAL MEDIA LINKS</h3>
+              <ul>
+                <li className={styles.links}>
+                  <a href="/#">Addrses</a>
+                  <br />
+                  <a href="/#">Phone Number</a>
+                  <br />
+                  <a href="/#">Email</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </footer>
